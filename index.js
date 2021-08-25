1b2ed692ec0753c329ae90f5a4852790f0579658
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-08-25 14:20:10 -0400

    Collage de imagenes listo

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express();
+const expressFileUpload = require('express-fileupload')
+const fs=require('fs')
+
+app.listen(3000, () => console.log("Servidor encendido en el puerto 3000"))
+app.use(express.static("deleteImg"))
+app.use(expressFileUpload({
+    limits: {
+        fileSize: 5000000
+    },
+    abortOnLimit: true,
+    responseOnLimit: "El peso de la imagen supera los limites permitidos"
+}))
+//me muestra el formulario para ingresar las imagenes
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/formulario.html")
+})
+app.get("/collage", (req, res) => {
+    res.sendFile(__dirname + "/collage.html")
+})
+//me envia la información de la imagen por POST, ruta /imagen
+app.post("/imagen", (req, res) => {  
+    const {
+        target_file
+    } = req.files;
+    const {
+        posicion
+    } = req.body
+    let {
+        name
+    } = target_file
+    name=`imagen-${posicion}.jpg`   
+    target_file.mv(`${__dirname}/deleteImg/${name}`, (err) => {
+        res.sendFile(__dirname+"/collage.html")
+    })
+})
+
+
+//muestro la información del formulario
+app.get("/deleteImg/:nombre",function(req,res){
+
+    const{nombre}=req.params;
+    console.log("nombre",nombre)
+    fs.unlink(`${__dirname}/deleteImg/${nombre}`,(err)=>{
+        res.send(`imagen ${nombre} fue eliminada con exito`)
+    })
+    
+
+})
